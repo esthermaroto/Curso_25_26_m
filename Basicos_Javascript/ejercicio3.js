8d177348d8d826758ae7233cf4578e1a0c8a3294
esthermaroto <esther.maroto.torres@gmail.com>
2025-09-22 16:45:15 +0200

    make a deposit function

--- Basicos_Javascript/ejercicio3.js
+++ Basicos_Javascript/ejercicio3.js
@@ -3,8 +3,8 @@
 
 Se pretende crear un pequeño sistema bancario en js
 -[x] 1.crear cuentas con titular y saldo
--[] 2.depositar dinero en una cuenta
--[] 3.retirar dinero de una cuentaI(cuando tenga saldo positivo)
+-[x] 2.depositar dinero en una cuenta
+-[] 3.retirar dinero de una cuenta(cuando tenga saldo positivo)
 -[] 4.consultar el saldo de una cuenta
 -[] 5.transferir dinero de una cuenta a otra(validando que la cuenta origen tenga saldo positivo)
 -[] 6.mantener un listado de cuentas y buscar una cuenta por su titular
@@ -40,10 +40,14 @@ let cuentaDestino = null;
  * @returns {string} - número de cuenta aleatorio con los campos del IBAN prestablecidos
  */
 function generarNumeroCuenta() {
-    for (let i = 0; i < longitudIBAN; i++) {
-        numeroCuenta += numeros.charAt(Math.floor(Math.random() * numeros.length));
+    if (numeroCuentaGenerado.length != 24) {
+        for (let i = 0; i < longitudIBAN; i++) {
+            numeroCuenta += numeros.charAt(Math.floor(Math.random() * numeros.length));
+        }
+    return IBAN = letras + ibanDigControl + ibanEntidad + ibanOficina + ibanDC + numeroCuenta;
+    } else {
+        return numeroCuentaGenerado;
     }
-    return letras + ibanDigControl + ibanEntidad + ibanOficina + ibanDC + numeroCuenta;
 }
 
 //Crear cuenta
@@ -52,7 +56,7 @@ function generarNumeroCuenta() {
  * @param {string} titular - nombre del titular de la cuenta
  */
 function crearCuenta(titular) {
-    numeroCuentaGenerado = generarNumeroCuenta();
+    numeroCuentaGenerado = IBAN;
     const nuevaCuenta = {
         titular: titular,
         numeroCuenta: numeroCuentaGenerado,
@@ -60,6 +64,28 @@ function crearCuenta(titular) {
     };
     cuentas.push(nuevaCuenta);
 }
+
+//Depositar dinero
+/**
+ * Añade una cantidad de dinero a una cuenta bancaria
+ * @param {string} numeroCuenta - número de cuenta donde se va a depositar el dinero
+ * @param {number} cantidad - cantidad de dinero a depositar
+ */
+function depositarDinero(numeroCuenta, cantidad) {
+    cuenta = cuentas.find(cuenta => cuenta.numeroCuenta === numeroCuenta);
+    if (cuenta) {
+        if (cantidad > 0) {
+        cuenta.saldo += cantidad;
+        console.log(`Se han depositado ${cantidad} en la cuenta ${numeroCuenta}. Nuevo saldo: ${cuenta.saldo}`);
+        } else {
+            console.log("La cantidad a depositar debe ser mayor que 0");
+        }
+    } else {
+        console.log("Cuenta no encontrada");
+    }
+}
+
+
 
 
 
@@ -73,4 +99,7 @@ function crearCuenta(titular) {
 console.log(generarNumeroCuenta());
 //test de la funcion crearCuenta
 crearCuenta("Juan");
+console.log(cuentas);
+//test de la funcion depositarDinero
+depositarDinero(cuentas[0].numeroCuenta, 100);
 console.log(cuentas);
